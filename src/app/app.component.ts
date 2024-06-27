c2ee2b4b6dfd73cfa3027c95dedc28991a5c0023
Luke Silver <luke.silver@keyrus.co.za>
2024-06-27 11:04:54 +0200

    changes to make it work with updates since tutorial and for keyrus azure tenant

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { MsalBroadcastService, MsalService } from '@azure/msal-angular';
+import { AuthenticationResult, EventMessage, EventType, InteractionStatus } from '@azure/msal-browser';
+import { Subject, filter, takeUntil } from 'rxjs';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-    title = 'msal-angular demo';
-    activeUser: string | undefined = "unknown user";
+export class AppComponent implements OnInit, OnDestroy {
+  title = 'msal-angular demo';
+  activeUser: string | undefined = 'unknown user';
+  isAuthenticated = false;
+
+  private unsubscribe = new Subject<void>();
+
+  constructor(private msalService: MsalService, private msalBroadcastService : MsalBroadcastService) {}
+
+  ngOnInit(): void {
+    this.msalBroadcastService.inProgress$.pipe(
+      filter((status: InteractionStatus) => status === InteractionStatus.None),
+      takeUntil(this.unsubscribe)
+    )
+    .subscribe(() => {
+      this.setAuthenticationStatus();
+    });
+
+    this.msalService.initialize();
+
+    this.msalBroadcastService.msalSubject$.pipe(
+      filter((message: EventMessage) => message.eventType === EventType.LOGIN_SUCCESS),
+      takeUntil(this.unsubscribe)
+    )
+    .subscribe((message: EventMessage)=>{
+      const authenticationResult = message.payload as AuthenticationResult;
+      this.msalService.instance.setActiveAccount(authenticationResult.account);
+    })
+
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe.next(undefined);
+
+  }
+
+  login(): void {
+    // this.msalService.instance.loginPopup({scopes: ['User.Read']})
+    this.msalService.instance.loginRedirect({
+      scopes: ['User.Read']
+    });
+  }
+
+  logout(): void {
+    this.msalService.instance.logoutRedirect();
+  }
+
+  setAuthenticationStatus(): void {
+    let activeAccount = this.msalService.instance.getActiveAccount();
+
+    if (!activeAccount && this.msalService.instance.getAllAccounts().length > 0)
+    {
+      activeAccount = this.msalService.instance.getAllAccounts()[0];
+      this.msalService.instance.setActiveAccount(activeAccount);
+    }
+
+    this.isAuthenticated = !!activeAccount;
+    this.activeUser = activeAccount?.username;
+    // this.isAuthenticated = this.msalService.instance.getAllAccounts().length > 0;
+    // this.activeUser = this.isAuthenticated ? this.msalService.instance.getAllAccounts()[0].username : 'unknown user';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Provider } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,6 +14,49 @@ import { MatCardModule } from '@angular/material/card';
 
 import { HomeComponent } from './home/home.component';
 import { ProfileComponent } from './profile/profile.component';
+import { MSAL_GUARD_CONFIG, MSAL_INSTANCE, MSAL_INTERCEPTOR_CONFIG, MsalBroadcastService, MsalGuard, MsalGuardConfiguration, MsalInterceptor, MsalInterceptorConfiguration, MsalRedirectComponent, MsalService, ProtectedResourceScopes } from '@azure/msal-angular';
+import { IPublicClientApplication, InteractionType, PublicClientApplication } from '@azure/msal-browser';
+
+function MSALInstanceFactory(): IPublicClientApplication {
+    return new PublicClientApplication({
+        auth: {
+            clientId: 'f9281ae8-a927-49ff-a151-82b31a5c915c',
+            authority: 'https://login.microsoftonline.com/bdeade0f-980c-4f3e-840c-c35ace5ea11e',
+            redirectUri: '/auth',
+        },
+        cache: {
+            cacheLocation: 'sessionStorage',
+            storeAuthStateInCookie: false,
+        }
+        // cache: {
+        //     cacheLocation: BrowserCacheLocation.LocalStorage,
+        //     storeAuthStateInCookie: isIE, // set to true for IE 11
+        // }
+    });
+}
+
+function MsalGuardConfigFactory(): MsalGuardConfiguration {
+    return {
+        interactionType: InteractionType.Redirect,
+        authRequest: {
+            scopes: ['User.Read'],
+        }
+    };
+}
+
+function MsalInterceptorConfigFactory(): MsalInterceptorConfiguration {
+    const myProtectedResourceMap = new Map<string, Array<string| ProtectedResourceScopes> | null>();
+    myProtectedResourceMap.set('https://graph.microsoft.com/v1.0/me', [{
+      httpMethod: 'GET',
+      scopes: ['User.Read']
+    }]);
+
+    return {
+        interactionType: InteractionType.Popup,
+        protectedResourceMap: myProtectedResourceMap
+    };
+}
+
 
 @NgModule({
     declarations: [
@@ -32,7 +75,28 @@ import { ProfileComponent } from './profile/profile.component';
         MatMenuModule,
         MatCardModule
     ],
-    providers: [],
-    bootstrap: [AppComponent]
+    providers: [
+      {
+        provide: MSAL_INSTANCE,
+        useFactory: MSALInstanceFactory
+      },
+      {
+        provide: MSAL_GUARD_CONFIG,
+        useFactory: MsalGuardConfigFactory
+      },
+      {
+        provide: HTTP_INTERCEPTORS,
+        useClass: MsalInterceptor,
+        multi: true
+      },
+      {
+        provide: MSAL_INTERCEPTOR_CONFIG,
+        useFactory: MsalInterceptorConfigFactory
+      },
+      MsalService,
+      MsalBroadcastService,
+      MsalGuard
+    ],
+    bootstrap: [AppComponent, MsalRedirectComponent]
 })
 export class AppModule { }
